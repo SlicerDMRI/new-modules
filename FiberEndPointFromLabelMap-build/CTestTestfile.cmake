fb652273eeea17b72895a21abd6bc42bd87332d5
wuye4567 <amri.wuye@gmail.com>
2016-12-15 16:08:24 -0500

    Remove the now ignored directory "**-build"

--- FiberEndPointFromLabelMap-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brain/Sandbox/FiberEndPointFromLabelMap
-# Build directory: /home/brain/Sandbox/FiberEndPointFromLabelMap-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(Testing)
